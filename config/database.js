004909ae1f9c7fb72de138c63e25a585d288bfda
scm-wutyitheint <scm.dott.developer10@gmail.com>
2021-09-05 16:46:07 +0630

    fix database

--- config/database.js
+++ config/database.js
@@ -14,11 +14,14 @@ module.exports = ({ env }) => {
           port: config.port,
           database: config.database,
           username: config.user,
-          password: config.password
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+          },  
         },
         options: {
-          ssl: false
-        }
+          ssl: env.bool('DATABASE_SSL', false),
+        },
       }
     }
   }
