d8026ee258ae28cdb69304a7399d44d8dae678d0
scm-wutyitheint <scm.dott.developer10@gmail.com>
2021-09-05 17:33:31 +0630

    change setting

--- config/database.js
+++ config/database.js
@@ -10,11 +10,11 @@ module.exports = ({ env }) => {
         connector: 'bookshelf',
         settings: {
           client: 'postgres',
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          username: config.user,
-          password: config.password,
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          username: env('DATABASE_USERNAME', ''),
+          password: env('DATABASE_PASSWORD', ''), 
           ssl: {
             rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
           },  
--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  // port: env.int('PORT', 1337),
-  port: process.env.PORT || env.int('PORT', 1337),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '83e73d4d6f41e41c446b912ee5ed7495'),
