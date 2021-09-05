05bff0e5f204caa6f527daeeebcdca7513e96cce
scm-wutyitheint <scm.dott.developer10@gmail.com>
2021-09-05 20:52:37 +0630

    fix setting

--- config/database.js
+++ config/database.js
@@ -1,29 +1,4 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-
-  if(env('NODE_ENV') === 'production'){
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection : 'default',
-      connections: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'postgres',
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          username: config.user,
-          password: config.password,
-          ssl: { rejectUnauthorized: false } 
-        },
-        options: {
-          ssl: false
-        },
-      }
-    }
-  }
-  return {
+module.exports = ({ env }) => ({
   defaultConnection: 'default',
     connections: {
       default: {
@@ -36,6 +11,5 @@ module.exports = ({ env }) => {
           useNullAsDefault: true,
         },
       },
-    },
-  }
-};
+    }
+});
