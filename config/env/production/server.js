45e13b2cd5ec58402b9c131f9e2cc018969ce993
scm-wutyitheint <scm.dott.developer10@gmail.com>
2021-09-23 21:42:33 +0630

    add port change

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,9 @@
 module.exports = ({ env }) => ({
   host: '0.0.0.0',
-  port: env.int('NODE_PORT', 1337),
-admin: {
-autoopen: false,
-},
+  // port: env.int('NODE_PORT', 1337),
+  port: process.env.PORT || env.int('PORT', 1337),
+  admin: {
+  autoopen: false,
+  },
 });
  
